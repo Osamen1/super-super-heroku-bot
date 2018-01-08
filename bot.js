a5e7437e59338ddc29c99ac284f7165afa8c0a8f
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-08 15:53:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,20 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("message", message => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.channel.type === "dm") return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        let commandFile = require(`./commands/${command}.js`);
+        commandFile.run(client, message, args);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
